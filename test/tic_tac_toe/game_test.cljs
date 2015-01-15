25c28ad7aa0773279e5bfd51971279b4d2bbdc0f
miguel <miguelvilag@gmail.com>
2015-01-14 22:13:11 -0500

    Adds more tests

--- test/tic_tac_toe/game_test.cljs
+++ test/tic_tac_toe/game_test.cljs
@@ -50,6 +50,26 @@
                       [{:x 1 :y 0 :mark "O"},{:x 1 :y 1 :mark "X"},{:x 1 :y 2 :mark "O"}]
                       [{:x 2 :y 0 :mark "X"},{:x 2 :y 1},{:x 2 :y 2 :mark "O"}]]})
 
+(def t0 [
+         [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "X"}],
+         [{:x 1 :y 0 :mark "O"},{:x 1 :y 1 :mark "X"},{:x 1 :y 2}]
+         [{:x 2 :y 0 :mark "X"},{:x 2 :y 1 :mark "X"},{:x 2 :y 2 :mark "O"}]])
+
+(def t1 [
+         [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "O"}],
+         [{:x 1 :y 0 :mark "O"},{:x 1 :y 1 :mark "O"},{:x 1 :y 2}]
+         [{:x 2 :y 0 :mark "O"},{:x 2 :y 1 :mark "X"},{:x 2 :y 2 :mark "O"}]])
+
+(def t2 [
+         [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "X"}],
+         [{:x 1 :y 0 :mark "O"},{:x 1 :y 1 :mark "X"},{:x 1 :y 2}]
+         [{:x 2 :y 0 :mark "O"},{:x 2 :y 1 :mark "X"},{:x 2 :y 2 :mark "X"}]])
+
+(def t3 [
+         [{:x 0 :y 0 :mark "O"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "O"}],
+         [{:x 1 :y 0 :mark "O"},{:x 1 :y 1 :mark "O"},{:x 1 :y 2}]
+         [{:x 2 :y 0 :mark "X"},{:x 2 :y 1 :mark "X"},{:x 2 :y 2 :mark "O"}]])
+
 (deftest base
   (testing "same-mark?"
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "X"},{:x 0 :y 2 :mark "X"}]) "X"))
@@ -58,4 +78,10 @@
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "O"},{:x 0 :y 1 :mark "X"},{:x 0 :y 2 :mark "O"}]) nil))
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "O"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "O"}]) "O")))
   (testing "has-same-mark-in-NE-SW-diag?"
-    (is (= (game/has-same-mark-in-NE-SW-diag? (:tiles g2)) nil))))
+    (is (= (game/has-same-mark-in-NE-SW-diag? (:tiles g2)) nil))
+    (is (= (game/has-same-mark-in-NE-SW-diag? t0) {:winner "X" :position {:d :NE-SW}}))
+    (is (= (game/has-same-mark-in-NE-SW-diag? t1) {:winner "O" :position {:d :NE-SW}})))
+  (testing "has-same-mark-in-NW-SE-diag?"
+    (is (= (game/has-same-mark-in-NW-SE-diag? (:tiles g2)) nil))
+    (is (= (game/has-same-mark-in-NW-SE-diag? t2) "X"))
+    (is (= (game/has-same-mark-in-NW-SE-diag? t3) "O"))))
