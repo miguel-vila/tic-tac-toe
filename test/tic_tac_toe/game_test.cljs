523260b5875a9c54903a88bde4217d4925d48b12
miguel <miguelvilag@gmail.com>
2015-01-14 22:21:28 -0500

    Fixes tests

--- test/tic_tac_toe/game_test.cljs
+++ test/tic_tac_toe/game_test.cljs
@@ -71,17 +71,20 @@
          [{:x 2 :y 0 :mark "X"},{:x 2 :y 1 :mark "X"},{:x 2 :y 2 :mark "O"}]])
 
 (deftest base
+
   (testing "same-mark?"
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "X"},{:x 0 :y 2 :mark "X"}]) "X"))
     (is (= (game/same-mark? [{:x 0 :y 0},{:x 0 :y 1},{:x 0 :y 2}]) nil))
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "X"},{:x 0 :y 1 :mark "X"},{:x 0 :y 2}]) nil))
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "O"},{:x 0 :y 1 :mark "X"},{:x 0 :y 2 :mark "O"}]) nil))
     (is (= (game/same-mark? [{:x 0 :y 0 :mark "O"},{:x 0 :y 1 :mark "O"},{:x 0 :y 2 :mark "O"}]) "O")))
+
   (testing "has-same-mark-in-NE-SW-diag?"
     (is (= (game/has-same-mark-in-NE-SW-diag? (:tiles g2)) nil))
     (is (= (game/has-same-mark-in-NE-SW-diag? t0) {:winner "X" :position {:d :NE-SW}}))
     (is (= (game/has-same-mark-in-NE-SW-diag? t1) {:winner "O" :position {:d :NE-SW}})))
+
   (testing "has-same-mark-in-NW-SE-diag?"
     (is (= (game/has-same-mark-in-NW-SE-diag? (:tiles g2)) nil))
-    (is (= (game/has-same-mark-in-NW-SE-diag? t2) "X"))
-    (is (= (game/has-same-mark-in-NW-SE-diag? t3) "O"))))
+    (is (= (game/has-same-mark-in-NW-SE-diag? t2) {:winner "X" :position {:d :NW-SE}}))
+    (is (= (game/has-same-mark-in-NW-SE-diag? t3) {:winner "O" :position {:d :NW-SE}}))))
