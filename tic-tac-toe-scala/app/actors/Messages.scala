5f5ab4d064bbc8d44099a3a5728c1fc7746a8e66
miguel <miguelvilag@gmail.com>
2015-01-25 12:11:28 -0500

    Better messages structure

--- tic-tac-toe-scala/app/actors/Messages.scala
+++ tic-tac-toe-scala/app/actors/Messages.scala
@@ -6,15 +6,18 @@ import models._
 /**
  * Created by mglvl on 24/01/15.
  */
-case object StartGame
-case object NoPlayersAvailable
-case class AvailableGame(gameActor: ActorRef)
-case class PlayAtPosition(position: Position)
-class YouArePlayer(player: Player)
+sealed trait UserSentMessages
+case object StartGame extends UserSentMessages
+case class PlayAtPosition(position: Position) extends UserSentMessages
+
+sealed trait UserReceivedMessages
+case object NoPlayersAvailable extends UserReceivedMessages
+class YouArePlayer(player: Player) extends UserReceivedMessages
 case object YouArePlayerX extends YouArePlayer(PlayerX)
 case object YouArePlayerO extends YouArePlayer(PlayerO)
-case class GameStarted(gameActor: ActorRef)
-object Draw
-case class GameWon(winner: Winner)
-case object Wait
-case object MakeYourMove
+case class GameStarted(gameActor: ActorRef) extends UserReceivedMessages
+object Draw extends UserReceivedMessages
+case class GameWon(winner: Winner) extends UserReceivedMessages
+case object Wait extends UserReceivedMessages
+case object MakeYourMove extends UserReceivedMessages
+case class PlayerPutAMarkInPosition(player: Player, position: Position) extends UserReceivedMessages
