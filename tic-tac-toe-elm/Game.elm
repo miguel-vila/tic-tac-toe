41eccf5188ebd52916f870266b04b359b023be4e
miguel <miguelvilag@gmail.com>
2016-04-04 17:49:05 -0500

    port para mandar acciones de usuario

--- tic-tac-toe-elm/Game.elm
+++ tic-tac-toe-elm/Game.elm
@@ -4,9 +4,15 @@ import Html exposing (..)
 import Player exposing (Player(..), otherPlayer)
 import Html.Events as Events
 import StartApp.Simple 
+import StartApp
+import Signal exposing (Address, Signal, Mailbox, mailbox, send)
+import Effects exposing (Effects, Never)
 import Html.Attributes exposing (..)
+import Task exposing (Task)
 import Board
 import Maybe
+import Json.Encode exposing (string, int, null, Value)
+--import Json.Decode exposing (Decoder, decodeValue,)
 
 type alias Board = Board.Model
 
@@ -38,45 +44,70 @@ getBoard model =
     _                       -> Nothing
 
 initialModel : Model
-initialModel = NotConnected
+initialModel = NotStarted
 
 type alias NewGameInfo = { userPlayer : Player
                          , currentPlayer : Player
                          }
 
+type alias Position = { x : Int
+                      , y : Int 
+                      }
+
+type alias PlayerMovement = { player   : Player
+                            , position : Position
+                            }
+
 type Action = Connected
             | StartGame
             | GameStarted NewGameInfo
-            | PlayerClick Int Int Player
+            | PlayerClick PlayerMovement
             | GameWon Player
             | GameDraw
             | NoOp
 
-update : Action -> Model -> Model
+sendMessage : UserMessage -> Effects Action
+sendMessage message = Signal.send userMessagesMailbox.address message
+                    |> Effects.task
+                    |> Effects.map (\_ -> NoOp)
+
+update : Action -> Model -> (Model, Effects Action)
 update action model =
   case (action,model) of
     (Connected, NotConnected) -> 
-      NotStarted
+      (NotStarted, Effects.none)
+    (StartGame, NotStarted) ->
+      (WaitingOtherPlayer, sendMessage JoinGame)
     (GameStarted gameStarted, NotStarted) -> 
-      StartedGame { userPlayer = gameStarted.userPlayer 
+      (StartedGame { userPlayer = gameStarted.userPlayer 
                   , currentPlayer = gameStarted.currentPlayer
                   , board = Board.initialModel (gameStarted.userPlayer /= gameStarted.currentPlayer)
                   }
-    (PlayerClick x y player, StartedGame startedGame) ->
-      if player == startedGame.currentPlayer then
-         let shouldBlock = player == startedGame.userPlayer
-             blockAction = if shouldBlock then Board.BlockBoard else Board.UnblockBoard
-             board' = Board.update (Board.BoardClick x y player) startedGame.board
-         in StartedGame { startedGame 
+      , Effects.none
+      )
+    (PlayerClick mov, StartedGame startedGame) ->
+      if mov.player == startedGame.currentPlayer then
+         let pos = mov.position
+             isUserMove = mov.player == startedGame.userPlayer
+             effects = if isUserMove then sendMessage (UserMovement pos) else Effects.none
+             blockAction = if isUserMove then Board.BlockBoard else Board.UnblockBoard
+             board' = Board.update (Board.BoardClick pos.x pos.y mov.player) startedGame.board
+         in (StartedGame { startedGame 
                           | board = Board.update blockAction board'
                           , currentPlayer = Player.otherPlayer startedGame.currentPlayer }
-      else model
+            , effects
+            )
+      else (model, Effects.none)
     (GameWon winner, StartedGame startedGame) ->
-      WonGame { userPlayer = startedGame.userPlayer, winner = winner, board = startedGame.board }
+      (WonGame { userPlayer = startedGame.userPlayer, winner = winner, board = startedGame.board }
+      , Effects.none
+      )
     (GameDraw, StartedGame startedGame) ->
-      DrawnGame { board = startedGame.board }
+      (DrawnGame { board = startedGame.board }
+      , Effects.none
+      )
     _ ->
-      model
+      (model, Effects.none)
 
 gameText msg = h2 [class "centered-text"] [ text msg ]
 
@@ -102,7 +133,7 @@ gameStatusView address model =
 fromBoardAction : Board.Action -> Action
 fromBoardAction boardAction =
   case boardAction of
-    Board.BoardClick x y player -> PlayerClick x y player
+    Board.BoardClick x y player -> PlayerClick { player = player , position = { x = x , y = y } }
     _                           -> NoOp
 
 boardView : Signal.Address Action -> Model -> List Html
@@ -117,5 +148,36 @@ view address model =
       ([ h1 [] [text "Tic Tac Toe"]
       , gameStatusView address model 
       ] ++ (boardView address model))
+
+type UserMessage = JoinGame
+                 | UserMovement Position
+                 | NoMessage
+
+userMessagesMailbox : Mailbox UserMessage
+userMessagesMailbox = mailbox NoMessage
+
+(=>) = (,)
+
+encodeMessage : UserMessage -> Value
+encodeMessage  message = 
+  case message of
+    JoinGame -> Json.Encode.object ["command" => string "StartGame"]
+    UserMovement position -> Json.Encode.object 
+                             [ "command"  => string "PlayAtPosition"
+                             , "position" => Json.Encode.object [ "x" => int position.x
+                                                                , "y" => int position.y]
+                             ]
+    NoMessage -> null
+
+port userMessages : Signal Value
+port userMessages = Signal.map encodeMessage userMessagesMailbox.signal
         
-main = StartApp.Simple.start { model = initialModel, update = update, view = view }
+--main = StartApp.Simple.start { model = initialModel, update = update, view = view }
+
+app = StartApp.start { init = (initialModel, Effects.none), update = update, view = view, inputs = [] }
+
+main = app.html
+
+-- "Materializa" side effects, por ej userMessages (por que es necesario?)
+port tasks : Signal (Task Never ())
+port tasks = app.tasks
